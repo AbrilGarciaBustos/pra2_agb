8c3ede1444e3d73da2e020cd3c823154d24e065f
AbrilGarciaBustos <20223tn053@utez.edu.mx>
2025-06-02 18:16:20 -0600

    [FIRST COMMIT SUBIDA INICIAL]

---
+++ src/main/java/utez/edu/mx/pra2_agb/Pra2AgbApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.pra2_agb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pra2AgbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pra2AgbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/utez/edu/mx/pra2_agb/Pra2AgbApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.pra2_agb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pra2AgbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
